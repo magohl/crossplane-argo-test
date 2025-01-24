13373e7a76f3ec1dc378cf1a6e32710a5288dd92
Magnus Ohlsson <magnus.ohlsson@gmail.com>
2025-01-24 07:21:39 +0000

    dddd

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,15 @@
+curl sS https://webi.sh/k9s | sh && source ~/.config/envman/PATH.env
+
+minikube start
+
+kubectl apply -k argocd/install
+
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-server --namespace argocd --timeout=300s
+
+kubectl apply -n argocd -f argocd/crossplane-bootstrap/crossplane-helm-secret.yaml
+
+kubectl apply -n argocd -f argocd/crossplane-bootstrap/crossplane.yaml
+
+kubectl apply -n argocd -f argocd/crossplane-bootstrap/crossplane.yaml
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
